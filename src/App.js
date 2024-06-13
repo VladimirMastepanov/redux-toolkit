82932b58ccee455c4731594cc3ba1ed30dd7aeeb
VladimirMastepanov <rozenkranc4@gmail.com>
2024-06-13 20:03:04 +0200

    delete comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement, incrementByAmount } from './slices/counterSlice';
 
-// import logo from './logo.svg';
-// import './App.css';
-
 function App() {
   const counter = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
@@ -23,22 +20,6 @@ function App() {
         <button onClick={() => dispatch(incrementByAmount(12))}>Прибавить 12</button>
       </div>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
